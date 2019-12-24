3357c4fa4ce8485dcaa976213a1bf04a05bad82e
melkam beyene <melkam3392beyu@gmail.com>
2019-12-23 22:28:16 -0800

    adding the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
+import './screens/home_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return MaterialApp(
+      title: "Flutter Demo",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor:Colors.red,
+        accentColor: Color(0XFFFEF9EB)
+      ),
+      home: HomeScreen(),
+    );
   }
 
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget{
+  @override
+  _HomeScreenState createState()=>_HomeScreenState();
+
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+     return Scaffold(
+       appBar: AppBar(
+         leading: IconButton(
+           icon: Icon(Icons.menu),
+           iconSize: 30.0,
+           color: Colors.white,
+           onPressed: (){},
+         ),
+         title: Text("Chats",style: TextStyle(fontSize: 28.0,fontWeight: FontWeight.bold),),
+       ),
+
+     );
+  }
+
+}
